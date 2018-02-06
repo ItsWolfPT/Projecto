fcb8cf4248da7d87e64a5ec5d7312f85ea54c621
Wolf <gosicwolf@gmail.com>
2018-02-06 10:36:23 +0000

    Add files via upload

--- src/projecto/Projecto.java
+++ src/projecto/Projecto.java
@@ -11,8 +11,12 @@ public class Projecto
     public static void main(String[] args) throws FileNotFoundException
     {
         final int TAMANHO =50;
+        String[] turma = new String[TAMANHO];
+        String[] numero = new String[TAMANHO];
         String[] nomes = new String[TAMANHO];
-        int[] notas = new int[TAMANHO];
+        int[] alg = new int[TAMANHO];
+        int[] java = new int[TAMANHO];
+        int[] vb = new int[TAMANHO];
         int nElems=0, op;
         do
         {
@@ -20,9 +24,12 @@ public class Projecto
           switch (op)
           {
              case 0:
+                  JOptionPane.showMessageDialog(null, "Fim...");
+                  break;
+             case 1:
                   if (nElems < TAMANHO)
                   {
-                    inserir(nomes, notas, nElems);
+                    inserir(turma, numero, nomes, alg, java, vb, nElems);
                     nElems++;
                   } 
                   else
@@ -30,30 +37,30 @@ public class Projecto
                       JOptionPane.showMessageDialog(null, "Atingida a capacidade maxima do vetor");
                   }
                   break;
-              case 1:
+              case 2:
                   if (nElems > 0)
                   {
-                    listar(nomes, notas, nElems);
+                    listar(turma, numero, nomes, alg, java, vb, nElems);
                   } 
                   else
                   {
                       JOptionPane.showMessageDialog(null, "Nao Existem alunos");
                   }
                   break; 
-              case 2:
+              case 3:
                   if (nElems > 0)
                   {
-                    atualizar(nomes, notas, nElems);
+                    atualizar(nomes, alg, java, vb, nElems);
                   } 
                   else
                   {
                       JOptionPane.showMessageDialog(null, "Nao Existem alunos");
                   }
                   break;
-              case 3:
+              case 4:
                   if (nElems>0)
                   {
-                      if(eliminar(nomes, notas, nElems)== true)
+                      if(eliminar(turma, numero, nomes, alg, java, vb, nElems)== true)
                       {
                       JOptionPane.showMessageDialog(null, "Aluno Eliminado");
                       nElems--;
@@ -68,70 +75,68 @@ public class Projecto
                       JOptionPane.showMessageDialog(null, "Nao existem alunos");
                   }   
                   break;
-              case 4:
+              case 5:
                   if (nElems > 0)
                   {
-                      ordenar(nomes, notas, nElems);
-                      listar(nomes, notas, nElems);
+                      ordenar(nomes, alg, java, vb, nElems);
+                      listar(turma, numero, nomes, alg, java, vb, nElems);
                   }
                   else
                   {
                       JOptionPane.showMessageDialog(null, "Nao exitem alunos");
                   }
                   break;
-              case 5:
+              case 6:
                   if(nElems > 0)
                   {
-                      gravar(nomes, notas, nElems);
+                      gravar(turma, numero, nomes, alg, java, vb, nElems);
                   }
                   else
                   {
                       JOptionPane.showMessageDialog(null, "Nao existem alunos");
                   }
-              case 6:
-                  
-                  
-              case 9:
-                  JOptionPane.showMessageDialog(null, "Fim...");
-                  break;
           }
         } 
-        while (op !=9);
+        while (op !=0);
     }
 
     private static int menu() 
     {
       int op;
       op = Integer.parseInt(JOptionPane.showInputDialog(
-        "0- Adicionar aluno\n"
-      + "1- Listar alunos\n" 
-      + "2- Atualizar nota de um aluno\n" 
-      + "3- Eliminar aluno\n"
-      + "4- Mostrar Informaçao ordenada por nome do aluno\n" 
-      + "5- Carregar dados para a memoria\n"
-      + "6-Gravar Dados em Ficheiro\n" 
-      + "9 - Sair\n"));
+        "1- Adicionar aluno\n"
+      + "2- Listar alunos\n" 
+      + "3- Atualizar nota de um aluno\n" 
+      + "4- Eliminar aluno\n"
+      + "5- Mostrar Informaçao ordenada por nome do aluno\n" 
+      + "6- Carregar dados para a memoria\n"
+      + "7-Gravar Dados em Ficheiro\n" 
+      + "0 - Sair\n"));
       return op;
     }
 
-    private static void inserir(String[] nomes, int[] notas, int nElems) 
+    private static void inserir(String[] turma, String[] numero, String[] nomes, int[] alg, int[] java,int[] vb, int nElems) 
     {
-        nomes[nElems] = JOptionPane.showInputDialog("Nome?");
-        notas[nElems] = Integer.parseInt(JOptionPane.showInputDialog("notas"));
+        turma[nElems] = JOptionPane.showInputDialog("Turma do Aluno?");
+        numero[nElems] = JOptionPane.showInputDialog("Numero do Aluno?");
+        nomes[nElems] = JOptionPane.showInputDialog("Nome do Aluno?");
+        alg[nElems] = Integer.parseInt(JOptionPane.showInputDialog("Insira a nota de Algoritmia"));
+        java[nElems] = Integer.parseInt(JOptionPane.showInputDialog("Insira a nota Java"));
+        vb[nElems] = Integer.parseInt(JOptionPane.showInputDialog("Insira a nota de Visual Basic"));
     }
 
-    private static void listar(String[] nomes, int[] notas, int nElems) 
+    private static void listar(String[] turma, String[] numero, String[] nomes, int[] alg, int[] java,int[] vb, int nElems) 
     {
         int x;
         String msg= "Listagem de alunos";
         for (x=0; x<nElems;x++)
         { 
-        msg+= "\n" + nomes[x] + "-" + notas[x];
+        msg+= "\n" + turma[x] + "-" + numero[x] + "-" + nomes[x] + "- Algoritmia -" + alg[x] + ", Java - " + java[x] + ", Visual Basic -" + vb[x];
         }
         JOptionPane.showMessageDialog(null, msg);
     }
 
-    private static void atualizar(String[] nomes, int[] notas, int nElems) 
+    private static void atualizar(String[] nomes,  int[] alg, int[] java,int[] vb, int nElems) 
     {
        String nome;
        int pos;
@@ -139,11 +144,13 @@ public class Projecto
        pos = pesquisar(nomes,nElems,nome);
        if (pos != -1)
        {
-            notas[pos] = Integer.parseInt(JOptionPane.showInputDialog("Qual o Novo nota?"));
+            alg[pos] = Integer.parseInt(JOptionPane.showInputDialog("Qual a nova nota a Algoritmia?"));
+            java[pos] = Integer.parseInt(JOptionPane.showInputDialog("Qual a nova nota a Java"));
+            vb[pos] = Integer.parseInt(JOptionPane.showInputDialog("Qual a nova nota a Visual Basic?"));
        }
        else
        {
-            JOptionPane.showMessageDialog(null,"Nao ha qualquer aluno com o nome introduzido");
+            JOptionPane.showMessageDialog(null,"Nao há qualquer aluno com o nome introduzido");
            
        }
     }
@@ -165,18 +172,22 @@ public class Projecto
         }
     }
 
-    private static boolean eliminar(String[] nomes, int[] notas, int nElems) 
+    private static boolean eliminar(String[] turma, String[] numero, String[] nomes, int[] alg, int[] java,int[] vb, int nElems) 
     {
         String nome;
         int pos,x;
-        nome =JOptionPane.showInputDialog("Qual é o funcinario que pretende Eliminar?");
+        nome =JOptionPane.showInputDialog("Qual é o aluno que pretende Eliminar?");
         pos = pesquisar(nomes,nElems, nome);
         if(pos != -1)
         {
             for (x=pos;x<nElems;x++)
             {
+                turma[x]=turma[x+1];
+                numero[x]=numero[x+1];
                 nomes[x]=nomes[x+1];
-                notas[x] = notas [x+1];
+                alg[x] = alg[x+1];
+                java[x] = java[x+1];
+                vb[x] = vb[x+1];
             }
             return true;
         }
@@ -186,7 +197,7 @@ public class Projecto
         }
     }
 
-    private static void ordenar(String[] nomes, int[] notas, int nElems) 
+    private static void ordenar(String[] nomes,  int[] alg, int[] java,int[] vb, int nElems) 
     {
         int x , y , aux1;
         String aux2;
@@ -199,16 +210,16 @@ public class Projecto
                     aux2=nomes[x];
                     nomes[x]=nomes[y];
                     nomes[y]=aux2;
-                    aux1= notas[x];
-                    notas[x]=notas[y];
-                    notas[y]=aux1;
+                    aux1= alg[x];
+                    alg[x]=alg[y];
+                    alg[y]=aux1;
                             
                 }
             }
         }
     }
 
-    private static void gravar(String[] nomes, int[] notas, int nElems) throws FileNotFoundException 
+    private static void gravar(String[] turma, String[] numero, String[] nomes, int[] alg, int[] java,int[] vb, int nElems) throws FileNotFoundException 
     {
         Formatter files3 = new Formatter(new File("alunos.txt"));
         int x;
@@ -216,17 +227,17 @@ public class Projecto
         {
             if(x==0)
             {
-                files3.format(nomes[x]+ ":" + notas[x]);
+                files3.format(turma[x] + "-" + numero[x] + "-" + nomes[x]+ ":" + alg[x] + "," + java[x] + "," + vb[x]);
             }
             else
             {
-                files3.format("\n" + nomes[x]+ ":" + notas[x]);
+                files3.format("\n" + turma[x] + "-" + numero[x] + "-" + nomes[x]+ ":" + alg[x] + "," + java[x] + "," + vb[x]);
             }
             files3.close();
         }
         
     }
-    private static int ler(String[] nomes, int[] notas, int nElems) throws FileNotFoundException {
+    private static int ler(String[] nomes, int[] alg, int[] java,int[] vb, int nElems) throws FileNotFoundException {
         Scanner fichFunc = new Scanner(new File("alunos.txt"));
         String linha;
         int pos, cont = 0;
@@ -237,7 +248,7 @@ public class Projecto
             pos = pesquisar(nomes, nElems, vetLinha[0]);
             if (pos == -1) {
                 nomes[nElems] = vetLinha[0];
-                notas[nElems] = Integer.parseInt(vetLinha[1]);
+                alg[nElems] = Integer.parseInt(vetLinha[1]);
                 nElems++;
                 cont++;
             }
